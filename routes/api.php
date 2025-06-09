9d1e7ef83a140febf1831fbe63223e6e2ecc81c4
Kamil Langer <kamillanger4@gmail.com>
2025-06-09 04:35:02 +0200

    feat: add auth routes and secure alert endpoints with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\PriceHistoryController;
 use App\Http\Controllers\Api\AlertController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,9 +13,15 @@ use App\Http\Controllers\Api\AlertController;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/user', [AuthController::class, 'user']);
+    });
+});
 
 // Products API
 Route::prefix('products')->group(function () {
@@ -24,13 +31,18 @@ Route::prefix('products')->group(function () {
 });
 
 // Alerts API
-Route::prefix('alerts')->group(function () {
-    Route::get('/', [AlertController::class, 'index']);
-    Route::post('/', [AlertController::class, 'store']);
-    Route::get('/statistics', [AlertController::class, 'statistics']);
-    Route::get('/{alert}', [AlertController::class, 'show']);
-    Route::put('/{alert}', [AlertController::class, 'update']);
-    Route::delete('/{alert}', [AlertController::class, 'destroy']);
-    Route::get('/{alert}/should-trigger', [AlertController::class, 'shouldTrigger']);
-    Route::post('/{alert}/trigger', [AlertController::class, 'trigger']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('alerts')->group(function () {
+        Route::get('/', [AlertController::class, 'index']);
+        Route::post('/', [AlertController::class, 'store']);
+        Route::get('/statistics', [AlertController::class, 'statistics']);
+
+        Route::middleware('alert.owner')->group(function () {
+            Route::get('/{alert}', [AlertController::class, 'show']);
+            Route::put('/{alert}', [AlertController::class, 'update']);
+            Route::delete('/{alert}', [AlertController::class, 'destroy']);
+            Route::get('/{alert}/should-trigger', [AlertController::class, 'shouldTrigger']);
+            Route::post('/{alert}/trigger', [AlertController::class, 'trigger']);
+        });
+    });
 });
